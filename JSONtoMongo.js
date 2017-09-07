657e2da2c59e03c9f079dc95a64b8a82bba148d2
stephlei <stephlei@ufl.edu>
2017-09-06 21:56:53 -0400

    Add files via upload

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -10,13 +10,36 @@ var fs = require('fs'),
     config = require('./config');
 
 /* Connect to your database */
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.db.uri,{useMongoClient:true});
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
+fs.readFile('./listings.json', 'utf8', function (err,data) {
+
+    console.log("importing data");
+    var i;
+    var dataList=JSON.parse(data);
+    var length=Object.keys(dataList.entries).length;
+
+    for(i = 0; i < length ; i++) {
+        var newListing = new Listing(dataList.entries[i]).save(function (err) {
+          if (err) throw err;
+        });
+    }
+});
+
+mongoose.disconnect();
 
 
+
+  /*
+    This callback function should save the data in the listingData variable, 
+    then start the server. 
+   */
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
   it saved everything correctly. 
--- queries.js
+++ queries.js
@@ -1,31 +1,66 @@
 /* Fill out these functions using Mongoose queries*/
+var fs = require('fs'),
+    mongoose = require('mongoose'), 
+    Schema = mongoose.Schema, 
+    Listing = require('./ListingSchema.js'), 
+    config = require('./config');
+
+/* Connect to your database */
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.db.uri,{ useMongoClient: true });
 
 var findLibraryWest = function() {
   /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
-};
+   Listing.find({name:'Library West'},function(error,listing){
+    if(error)
+      throw error;
+    console.log(listing);
+   }
+)};
 var removeCable = function() {
   /*
     Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+   Listing.findOneAndRemove({code:'CABL'},function(error,listing){
+    if(error)
+      throw error;
+    
+      console.log('Successfully removed!');
+    });
+   
 };
 var updatePhelpsLab = function() {
   /*
     Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+   Listing.findOneAndUpdate({name:'Phelps Laboratory'},{address:'1953 MUSEUM RD GAINESVILLE, FL 32611'},
+    function(error,listing){
+      if(error)
+        throw error;
+      console.log(listing);
+      console.log("Successfully updated!");
+    });
 };
+
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
-   */
+  */
+  Listing.find({}, function(err, Listing) {
+    if (err) throw err;
+    console.log(Listing);
+  })
 };
-
 findLibraryWest();
 removeCable();
 updatePhelpsLab();
 retrieveAllListings();
+
+
